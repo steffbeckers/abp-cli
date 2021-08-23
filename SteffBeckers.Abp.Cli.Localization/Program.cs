4379820e93439bf96fea1c8c8527a0e450de831b
Steff Beckers <steff@steffbeckers.eu>
2021-08-23 23:40:10 +0200

    Initial commit

---
+++ SteffBeckers.Abp.Cli.Localization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SteffBeckers.Abp.Cli.Localization
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
