70ce7ae95c39d4b628e15cbf2b73b610d599ac7b
Steff Beckers <steff@steffbeckers.eu>
2021-08-24 00:02:17 +0200

    Initial tests with System.CommandLine

--- SteffBeckers.Abp.Cli.Localization/Program.cs
+++ SteffBeckers.Abp.Cli.Localization/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SteffBeckers.Abp.Cli.Localization
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RootCommand rootCommand = new RootCommand("ABP.io Localization CLI");
+
+            Command listCommand = new Command("list", "Lists all localization files in directory.");
+            listCommand.Handler = CommandHandler.Create(() =>
+            {
+                List<string> filePaths = Directory.GetFiles(Directory.GetCurrentDirectory())
+                    .Where(x => x.EndsWith(".json"))
+                    .ToList();
+
+                filePaths.ForEach(x => Console.WriteLine(x));
+            });
+            rootCommand.AddCommand(listCommand);
+
+            return await rootCommand.InvokeAsync(args);
         }
     }
 }
