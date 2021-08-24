ecbb7d8e34304fa0034fe5a272f302dcb6548f5d
Steff Beckers <steff@steffbeckers.eu>
2021-08-24 19:59:08 +0200

    Updates on localization export to excel

---
+++ SteffBeckers.Abp.Cli/Localization/Models/LocalizationFile.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SteffBeckers.Abp.Cli.Localization.Models
+{
+    public class LocalizationFile
+    {
+        [JsonIgnore]
+        public string Path { get; set; }
+
+        [JsonProperty("culture")]
+        public string Culture { get; set; }
+
+        [JsonProperty("texts")]
+        public Dictionary<string, string> Texts { get; set; }
+    }
+}
--- SteffBeckers.Abp.Cli/Program.cs
+++ SteffBeckers.Abp.Cli/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Packaging;
+using Newtonsoft.Json;
+using SteffBeckers.Abp.Cli.Localization.Models;
+using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
@@ -14,18 +17,56 @@ namespace SteffBeckers.Abp.Cli
         {
             RootCommand rootCommand = new RootCommand("Steff's ABP.io CLI");
 
-            Command listCommand = new Command("list", "Lists all localization files in directory.");
-            listCommand.Handler = CommandHandler.Create(() =>
-            {
-                List<string> filePaths = Directory.GetFiles(Directory.GetCurrentDirectory())
-                    .Where(x => x.EndsWith(".json"))
-                    .ToList();
+            Command localizationCommand = new Command("localization", "Localization commands.");
+            Command localizationExportCommand = new Command("export", "Export localization files to other formats.");
+            Command localizationExportExcelCommand = new Command("excel", "Export to Excel.");
 
-                filePaths.ForEach(x => Console.WriteLine(x));
+            localizationExportExcelCommand.Handler = CommandHandler.Create(async () =>
+            {
+                List<LocalizationFile> localizationFiles = await GetLocalizationFiles();
+
+                string directoryName = new DirectoryInfo(Path.GetDirectoryName(localizationFiles.First().Path)).Name;
+                string spreadsheetFileName = $"{directoryName}.xslx";
+
+                await File.WriteAllTextAsync(spreadsheetFileName, "Test");
             });
-            rootCommand.AddCommand(listCommand);
+
+            localizationExportCommand.AddCommand(localizationExportExcelCommand);
+            localizationCommand.AddCommand(localizationExportCommand);
+            rootCommand.AddCommand(localizationCommand);
 
             return await rootCommand.InvokeAsync(args);
+        }
+
+        private static async Task<List<LocalizationFile>> GetLocalizationFiles()
+        {
+            List<LocalizationFile> localizationFiles = new List<LocalizationFile>();
+
+            List<string> localizationFilePaths = Directory.GetFiles(Directory.GetCurrentDirectory())
+                .Where(x => x.EndsWith(".json"))
+                .ToList();
+
+            foreach (string localizationFilePath in localizationFilePaths)
+            {
+                string localizationFileJson = await File.ReadAllTextAsync(localizationFilePath);
+
+                LocalizationFile localizationFile = JsonConvert.DeserializeObject<LocalizationFile>(localizationFileJson);
+                if (localizationFile.Culture == null)
+                {
+                    continue;
+                }
+
+                localizationFile.Path = localizationFilePath;
+
+                localizationFiles.Add(localizationFile);
+            }
+
+            if (localizationFiles.Count == 0)
+            {
+                throw new Exception("No localization files found.");
+            }
+
+            return localizationFiles;
         }
     }
 }
